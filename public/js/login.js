7e9e9d6fefeaea62898e53bc19df5f40b3032abf
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2020-04-26 09:00:47 +0530

    b

---
+++ public/js/login.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+import '@babel/polyfill'; 
+import { showAlert } from './alerts';
+import axios from 'axios';
+
+export const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+
+    if(res.data.status === 'success') {
+        showAlert('success', 'Logged in successfully!');
+        window.setTimeout(() => {
+            // to send the user to another page
+            location.assign('/');
+        }, 1500);
+    }
+  } catch (err) {
+    showAlert('error' ,err.response.data.message);
+  }
+};
+
+export const logout = async () => {
+  try{
+    const res = await axios({
+      method: 'GET',
+      url: '/api/v1/users/logout'
+    })
+
+    if ((res.data.status = 'success')) location.reload(true);
+  } catch(error) {
+    showAlert('error', 'Error logging out! Try again');
+  }
+}
