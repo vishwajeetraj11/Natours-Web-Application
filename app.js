6606d58fba8e04c2bcc06d9458e43054cb02ebb6
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2020-05-23 00:25:48 +0530

    updated app.js

--- app.js
+++ app.js
@@ -91,30 +91,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Single entrypoint file location:
-// const entryFiles = path.join(__dirname, './public/js/index.js');
-// OR: Multiple files with globbing (can also be .js)
-// const entryFiles = './src/*.js';
-// OR: Multiple files in an array
-// const entryFiles = ['./src/index.html', './some/other/directory/scripts.js'];
-
-// // Bundler options
-// const options = {
-//   outDir: './dist', // The out directory to put the build files in, defaults to dist
-//   outFile: 'bundle.js', // The name of the outputFile
-//   publicUrl: '/', // The url to serve on, defaults to '/'
-//   watch: true // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
-// };
-
-// (async function() {
-//   // Initializes a bundler using the entrypoint location and options provided
-//   const bundler = new Bundler(entryFiles, options);
-
-//   // Run the bundler, this returns the main bundle
-//   // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
-//    await bundler.bundle();
-// })();
-
 // 2. Routes
 
 app.use('/', viewRouter);
